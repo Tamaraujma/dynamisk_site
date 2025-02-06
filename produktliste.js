21a3ad1f868cb5fe3cdd7226791b52da1fadca0f
Tamara Ujmajuridze <Tauj0001@stud.kea.dk>
2025-02-06 09:59:20 +0100

    hover

--- produktliste.js
+++ produktliste.js
@@ -18,7 +18,8 @@ function showList(products) {
         <a href="produkt.html?productId=${product.id}">
           <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="${product.basecolour}" />
         </a>
-        <h4>${product.productdisplayname}</h4>
+<a href="produkt.html?productId=${product.id}">
+        <h4>${product.productdisplayname}</h4></a>
         <p>${product.articletype}</p>
         <p>DKK ${product.price},-</p>
         <p class="tilbud ${product.discount && "isOnSale"}">-${product.discount}%</p>
